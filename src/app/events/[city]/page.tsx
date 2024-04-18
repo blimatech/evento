cb3275e86e373f1ff7754603e7cb6ee239367eda
blimatech <tecnica.blima@gmail.com>
2024-04-18 11:36:15 +0100

    Events route

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,7 +1,22 @@
-const EventsPage = () => {
-    return (
-        <main>EventsPage</main>
-    );
+import H1 from "@/components/H1";
+
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+const EventsPage = ({ params }: EventsPageProps) => {
+  const city = params.city;
+  return (
+    <main className="flex min-h-[110vh] flex-col items-center px-[20px] py-24">
+      <H1>
+        {city === "all" && "All events"}
+        {city !== "all" &&
+          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
+    </main>
+  );
 };
 
 export default EventsPage;
